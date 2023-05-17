c258bcac308eb95357b39db6c151145cea04a1e0
jarenas12 <132343517+jarenas12@users.noreply.github.com>
2023-05-17 15:33:25 -0700

    Added typeCreate()

--- TransientTask.java
+++ TransientTask.java
@@ -5,13 +5,13 @@ public class TransientTask extends Task{
     // Inherits methods startTimeCreate, dateCreate, durationCreate, nameCreate, and taskTypeCreate
 
     // Transient Task constructor with arguements
-    public TransientTask(int inputDate, float inputTime, float inputDuration, String inputName)
+    public TransientTask(int inputDate, float inputTime, float inputDuration, String inputName, String type)
     {
         startTime = inputTime;
         date = inputDate;
         duration = inputDuration;
         name = inputName;
-        taskType = "Transient";
+        taskType = type;
     }
 
     // Transient Task constructor with no arguements
@@ -22,7 +22,33 @@ public class TransientTask extends Task{
         this.duration = this.durationCreate();
         this.name = this.nameCreate();
 
-        this.taskType = "Transient";
+        this.taskType = typeCreate();
+    }
+    // Attribute Methods ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public String typeCreate() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("\nChoose a recurring task type: {\"Visit\", \"Shopping\", \"Appointment\"}");
+        String userInput;
+        String[] typeList = {"Visit", "Shopping", "Appointment"};
+        boolean isValid = false;
+        while (true) {
+            System.out.println("Enter type: ");
+            userInput = scanner.nextLine();
+
+            for (String type : typeList) {
+                if (userInput.equals(type)) {
+                    isValid = true;
+                }
+            }
+
+            if (isValid) {
+                System.out.println("Selected type: " + userInput);
+                break;
+            } else {
+                System.out.println("Invalid type. Try again.");
+            }
+        }
+        return userInput;
     }
 
 }
