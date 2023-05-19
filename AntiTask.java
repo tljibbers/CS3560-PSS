52f7a889c990f08fc2279292d6a2fd1eabc8052c
jarenas12 <132343517+jarenas12@users.noreply.github.com>
2023-05-19 01:50:37 -0700

    Added javadocs

--- AntiTask.java
+++ AntiTask.java
@@ -3,7 +3,14 @@ public class AntiTask extends Task {
 
     // Inherits methods: startTimeCreate, dateCreate, durationCreate, nameCreate, and taskTypeCreate
 
-    // Anti Task constructor with arguments
+    // AntiTask constructor with arguments
+     /**
+     * Constructor of AntiTask that include parameters defined by the user.
+     * @param inputDate             The date of the task being cancelled
+     * @param inputTime             The time of the task being cancelled
+     * @param inputDuration         The duration of the task being cancelled
+     * @param inputName             The name specified by the user
+     */
     public AntiTask(int inputDate, float inputTime, float inputDuration, String inputName)
     {
         startTime = inputTime;
@@ -13,7 +20,14 @@ public class AntiTask extends Task {
         taskType = "Cancellation";
     }
 
-    // Anti Task constructor with no arguments
+    // AntiTask constructor with no arguments
+     /**
+     * Constructor of AntiTask (Contains no parameters. All attributes will come from user input and are verified through methods)
+     * inputDate                The date of the task being cancelled
+     * inputTime                The time of the task being cancelled
+     * inputDuration            The duration of the task being cancelled
+     * inputName                The name specified by the user
+     */
     public AntiTask()
     {
         System.out.println("DATE MUST MATCH THE RECURRING TASK INSTANCE:");
